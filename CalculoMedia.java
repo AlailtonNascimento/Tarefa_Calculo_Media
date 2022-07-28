562581531baef539dc045e5af5e78b769d564add
Alailton Nascimento <allayunascimento@gmail.com>
2022-07-28 14:04:09 -0300

    Atividade do modulo 8 do curso Java EBAC

---
+++ CalculoMedia.java
@@ -0,0 +1,13 @@
+public class CalculoMedia {
+
+    public static void main(String args[]){
+        int nota1 = 5;
+        int nota2 = 10;
+        int nota3 = 20;
+        int nota4 = 25;
+        float media;
+
+        media = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println("Media: " + media);
+    }
+}
